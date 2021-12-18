f1c189d2aea7402d5977369d9bcba8cb5927ba68
EosRicsi <90265234+EosRicsi@users.noreply.github.com>
2021-12-18 19:50:28 +0100

    Delete Fenykep directory

--- Fenykep/src/Persistence/ClientName/IClientName.java
+++
@@ -1,8 +0,0 @@
-package Persistence.ClientName;
-
-import Logic.Client;
-
-//A név megszerzéséhes tartozó interface egy metódusa
-public interface IClientName {
-    public String getClientName();
-}
